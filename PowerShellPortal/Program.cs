c38f1921bb01a014c9d6723eebf3f56df926802c
Ashley Strahle <a.strahle@me.com>
2022-05-26 22:05:07 +1000

    Fix to allow working on MacOS

--- PowerShellPortal/Program.cs
+++ PowerShellPortal/Program.cs
@@ -10,11 +10,6 @@ builder.Services.AddSignalR();
 builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
    .AddNegotiate();
 
-builder.Services.AddAuthorization(options =>
-{
-    // By default, all incoming requests will be authorized according to the default policy.
-    options.FallbackPolicy = options.DefaultPolicy;
-});
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
